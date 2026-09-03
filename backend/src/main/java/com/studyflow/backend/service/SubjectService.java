de98465f7f4be62c81411643cc9141a78fa4f1a1
Rosana Celine <celine.rosana@gmail.com>
2026-09-03 14:22:39 -0300

    feat: adiciona funcionalidade para listar todas as disciplinas

--- backend/src/main/java/com/studyflow/backend/service/SubjectService.java
+++ backend/src/main/java/com/studyflow/backend/service/SubjectService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -54,5 +55,11 @@ public class SubjectService {
         subjectRepository.delete(subject);
 
         return ResponseEntity.noContent().build();
+    }
+
+    public List<Subject> listAll () {
+        List<Subject> subjects = subjectRepository.findAll();
+
+        return subjects;
     }
 }
