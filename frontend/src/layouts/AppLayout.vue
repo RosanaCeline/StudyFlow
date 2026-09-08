85a83601c25e2a28a12b110c715a55137ec0de76
Rosana Celine <celine.rosana@gmail.com>
2026-09-08 12:10:51 -0300

    perf: adiciona KeepAlive no AppLayout para otimizar a navegação

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -3,13 +3,17 @@
 </script>
 
 <template>
-    <div class="d-flex min-vh-100">
+  <div class="d-flex min-vh-100">
 
-        <Sidebar />
+    <Sidebar />
 
-        <main class="flex-grow-1 p-4 bg-light overflow-auto">
-            <RouterView />
-        </main>
+    <main class="flex-grow-1 p-4 bg-light overflow-auto">
+      <RouterView v-slot="{ Component }">
+        <KeepAlive>
+          <component :is="Component" />
+        </KeepAlive>
+      </RouterView>
+    </main>
 
-    </div>
+  </div>
 </template>
